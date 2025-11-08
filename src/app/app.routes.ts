47bcb0fc816db75ccae77ec7b81a502221a70867
rubyesmelc0911 <rubyesme.lc0911@gmail.com>
2025-11-07 18:23:49 -0600

    Ruteo entre BG y ER

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { BalanceGeneralComponent } from './pages/balance-general/balance-general.component';
+import { EstadoResultadoComponent } from './pages/estado-resultado/estado-resultado.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'bg',
+    component: BalanceGeneralComponent
+  },
+  {
+    path: 'ER',
+    component: EstadoResultadoComponent
+  },
+  { //esto es para redirigir a la ruta original si se intenta acceder a una no definida
+    path: '**',
+    redirectTo: ''
   }
 
 ];
